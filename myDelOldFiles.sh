1bab6f2f70cedacd2b8dc46b6cd0d7d7d5eea56a
rgjoshi24 <rgjoshi24@gmail.com>
2017-03-09 21:08:22 -0500

    Del old files
    The script helps delete old files in a provided folder and no.of.days option.

---
+++ myDelOldFiles.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+Folder=$1
+NDays=$2
+
+if [ "$Folder" == "" ] || [ "$NDays" == "" ]
+then
+        echo "Either one of the parameter is blank"
+        echo "Usage is: $0 folder NoOfDays"
+        exit 1
+fi
+
+if [ ! -d $Folder ]
+then
+        echo "$Folder is not a folder"
+        echo "Usage is: $0 folder NoOfDays"
+        exit 2
+fi
+
+find $FOLDER/* -mtime +$N_DAYS -exec rm {} \;
